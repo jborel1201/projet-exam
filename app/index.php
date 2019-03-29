adea33ff6142d08afbbf7adb816c5894e3e1aae7
jborel1201 <j.borel1201@gmail.com>
2019-03-29 09:27:35 +0100

    debut projet final

--- app/index.php
+++ app/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="fr" ng-app="imgApp">
 
@@ -6,7 +5,7 @@
 
     <meta charset="UTF-8">
 
-    <title>Images</title>
+    <title>Images Php</title>
 
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -14,6 +13,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="../node_modules/bootswatch/dist/slate/bootstrap.css">
 
 
 
@@ -22,6 +22,7 @@
     <script src="lib/angular-route/angular-route.js"></script>
     <script src="lib/angular-resource/angular-resource.js"></script>
     <script src="lib/jquery/dist/jquery.min.js"></script>
+    
 
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -56,11 +57,8 @@
 
 <body ng-controller="mainController">
 
+    <select-bar></select-bar>  
 
-    <div id="dropfile">Upload</div>
-
-
-    <select-bar></select-bar>
     <div ng-view></div>
 
 
