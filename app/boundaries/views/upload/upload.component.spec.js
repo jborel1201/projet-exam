269c5956e3e45c834726c41b32bdc20ef335e9dd
jborel1201 <j.borel1201@gmail.com>
2019-04-05 16:54:31 +0200

    nouvelle tentative de test

--- app/boundaries/views/upload/upload.component.spec.js
+++ app/boundaries/views/upload/upload.component.spec.js
@@ -1,22 +1,27 @@
-describe('uploadController', function() {
+describe('controller : uploadController', function () {
     beforeEach(module('upload'));
-  
-    var $controller, $rootScope;
-  
-    beforeEach(inject(function(_$controller_, _$rootScope_){
-      // The injector unwraps the underscores (_) from around the parameter names when matching
-      $controller = _$controller_;
-      $rootScope = _$rootScope_;
+
+    var uploadController, scope;
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        scope = $rootScope.$new();
+        uploadController = $controller('uploadController', {
+            $scope: scope
+        });
     }));
-  
-    describe('$scope.removeAll', function() {
-      it('', function() {
-        var $scope = $rootScope;
-        var controller = $controller('uploadController', { $scope: $scope });
-        $scope.files = [1,2,3,4,5];
-       
-        $scope.removeAll();
-        expect($scope.file).toEqual('[]');
-      });
-    });
-  });
+
+    it('',function(){
+        expect($scope.files.length).toEqual(0);
+    })
+    /*describe('$scope.removeAll', function () {
+        it('', function () {
+            var $scope = $rootScope;
+            $controller('uploadController', { $scope: $scope });
+            $scope.files = [1, 2, 3, 4, 5];
+
+            $scope.removeAll();
+            expect($scope.file).toEqual('[]');
+        });
+    });*/
+});
