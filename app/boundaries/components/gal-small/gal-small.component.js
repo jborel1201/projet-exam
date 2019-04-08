f17de79603a24cb883d871acde176f72d394da06
jborel1201 <j.borel1201@gmail.com>
2019-04-08 16:42:36 +0200

    gal small design ok

--- app/boundaries/components/gal-small/gal-small.component.js
+++ app/boundaries/components/gal-small/gal-small.component.js
@@ -2,65 +2,83 @@ angular.
     module('galSmall').
     component('galSmall', {
         templateUrl: "boundaries/components/gal-small/gal-small.template.html",
-        controller: ['$scope', "$timeout",'Image', function smallController($scope, $timeout,Image) {
+        controller: function smallController(Image, $scope) {
 
-          
-            this.images = Image.query();
-          
+            var ctrl = this
 
-            // fonction de raffraichissement de la vue lors du drag and drop
-            function updateData(index) {
-                $scope.$apply(function () {
-                    images.splice(index, 1)
-                    objectData.images = images;
-                    this.data = objectData
+            ctrl.images = Image.query();
+            ctrl.selectedImage = null;
+
+
+            $scope.openMenu = function (event, image) {
+
+                // Récupération des données de l'élément selectionné
+                ctrl.selectedImage = image
+
+                // Récupération des élements du DOM pour calcul des positions
+                var menu = $('#menu-item');
+                var menuArea = $('#menu-area');
+                var menuAreaParent = $('#menu-area').parent();
+
+                //Affichage du menu
+                menu.removeClass("hidden");
+
+                //position click souris
+                let posX = event.pageX;
+                let posY = event.pageY;
+                //dimension du menu
+                let menuWidth = menu.innerWidth();
+                let menuHeight = menu.innerHeight();
+                //offset top du container
+                let areaOffsetTop = menuAreaParent[0].getBoundingClientRect().top;
+                //max bordure droite
+                let areaRight = menuArea[0].getBoundingClientRect().right;
+                //Calcul de la position prévu du menu
+                let menuTopPosition = posY - menuHeight;
+                let menuRightPosition = posX + menuWidth;
+
+
+                /*Vérification de la position droite du menu par rapport au bord droit de la div
+                 et ajustement si depasse*/
+                (menuRightPosition <= areaRight) ?
+                    menu.css({
+                        'left': posX + 'px'
+                    }) :
+                    menu.css({
+                        'left': posX - (menuRightPosition - areaRight) + 'px'
+                    });
+
+                /*Vérification de la position haute du menu par rapport a l'offsetTop de la div
+                et ajustement si depasse*/
+                (menuTopPosition >= areaOffsetTop) ?
+
+                    menu.css({
+                        'top': posY - menuHeight + 'px'
+                    }) :
+                    menu.css({
+                        'top': posY - menuHeight + (areaOffsetTop - menuTopPosition) + 'px'
+                    });
+
+                /*
+                Ajustement de la position du menu lors du scroll
+                */
+                let topInit = menu.offset().top;
+                $(menuAreaParent).scroll(function () {
+                    menu.css({
+                        'top': (topInit - (areaOffsetTop - menuArea[0].getBoundingClientRect().top)) + 'px'
+                    });
+                });
+
+                /*
+                annulation de la visibilité du menu lors du click sur un élément du DOM autre
+                qu'une icone de menu
+                */
+                $(document).on('click',function(){
+                    menu.addClass("hidden");
                 });
             }
 
 
+        }//ctrl
 
-            $(function () {
-                $timeout(function () {
-
-                    $("#draggableContainer").selectable({
-                        filter: "img",
-                        selected: function (event, ui) {
-                            console.log(ui)
-                        }
-                    })
-
-                    $(".draggable").draggable({
-                                revert: true,
-                                snap: "#droppable"
-                            });
-                            $("#draggableContainer").delegate(".draggable", "dblclick", function () {
-                            var imageName = $(this).find('img').attr('alt');
-                            document.location.href = `#!/view/${imageName}`
-                        });
-
-
-
-                    $("#droppable").droppable({
-                        drop: function (event, ui) {
-                            var imgDrop = JSON.parse(ui.draggable.context.dataset.object);
-                            //ajout de l'élément droppé à la liste des images deu scope principal
-                            $scope.$parent.$parent.imagesDrop.push(imgDrop);
-                            //récupération de l'index de l'élément et mise à jour de la liste d'images                   
-                            let indexDrop = ui.draggable.context.dataset.index;
-                            updateData(indexDrop);
-
-                        }
-
-
-                    });
-
-                });
-
-            });
-
-
-        }
-
-
-        ]
     });
--- app/boundaries/views/galerie/galerie.component.js
+++ app/boundaries/views/galerie/galerie.component.js
@@ -2,11 +2,10 @@ angular.
     module('galerie').
     component('galerie', {
         templateUrl: "boundaries/views/galerie/galerie.html",
-        controller: ['$scope', '$routeParams', function galerieController($scope, $routeParams) {
+        controller: function galerieController($scope, $routeParams) {
 
             $scope.size = $routeParams.size;
-            console.log($scope.size)
 
 
-        }]
+        }
     });
