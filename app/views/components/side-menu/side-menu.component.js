cda2b60973cd498106ae942e77f659f49ae9622d
jborel1201 <j.borel1201@gmail.com>
2019-04-18 23:04:41 +0200

    creation de la n,ouvelle bar de nav

---
+++ app/views/components/side-menu/side-menu.component.js
@@ -0,0 +1,10 @@
+angular.
+    module('sideMenu').
+    component('sideMenu', {
+        templateUrl: 'views/components/side-menu/side-menu.template.html',
+        controller: function sideMenuController() {
+            
+        }//Ctrl
+    });
+
+
